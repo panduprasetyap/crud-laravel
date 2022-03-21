03daa841d50321f87cdeba4e57c78eab10d74fa2
panduprasetyap <prasetya038@gmail.com>
2022-03-21 12:04:22 +0700

    update

---
+++ lavarel_crud/resources/views/blog.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Website Tutorial Pandu</title>
+</head>
+
+<body>
+  <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+  <p>Ini adalah view blog. ada di route blog.</p>
+</body>
+
+</html>
